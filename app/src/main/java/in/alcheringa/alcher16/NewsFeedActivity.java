a612ac0ba9cadcee73b8a399d0d43baf3708aa0c
hariharan <hariharan.mathavan@gmail.com>
2016-01-26 06:00:55 +0530

    NewsFeed Activity Complete

--- app/src/main/java/in/alcheringa/alcher16/NewsFeedActivity.java
+++ app/src/main/java/in/alcheringa/alcher16/NewsFeedActivity.java
@@ -1,19 +1,93 @@
 package in.alcheringa.alcher16;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class NewsFeedActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_feed);
         toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
         setSupportActionBar(toolbar);
+
+        Drawer drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withTranslucentStatusBar(false)
+                .withActionBarDrawerToggle(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .addDrawerItems(App.item1, App.item2,App.item3,App.item4,App.item5,App.item6,App.item7)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
+                        return true;
+                    }
+                })
+                .build();
+        drawer.setSelection(1);
+
+        listView=(ListView) findViewById(R.id.list_news);
+
+
+        File file = new File(Environment.getExternalStorageDirectory()+ "/alcheringa/pushlog.txt");
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            List<String> lines = new ArrayList<String>();
+            String line = null;
+
+            try {
+                while ((line = bufferedReader.readLine()) != null)
+                {
+                    lines.add(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Collections.reverse(lines);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(NewsFeedActivity.this,R.layout.module_list_item, R.id.list_item_text, lines);
+            listView.setAdapter(adapter);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
     }
 
     @Override
