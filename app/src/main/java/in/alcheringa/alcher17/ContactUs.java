e6d3387a630b1208f697eaab64976e6c50c16ec4
Arkadeep-sophoIITG <arkadeep@iitg.ernet.in>
2017-02-02 20:35:25 +0530

    Optimized and improved app

--- app/src/main/java/in/alcheringa/alcher17/ContactUs.java
+++ app/src/main/java/in/alcheringa/alcher17/ContactUs.java
@@ -1,8 +1,10 @@
 package in.alcheringa.alcher17;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -23,57 +25,6 @@ public class ContactUs extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
         setSupportActionBar(toolbar);
 
-//        Drawer drawer = new DrawerBuilder()
-//                .withActivity(this)
-//                .withToolbar(toolbar)
-//                .withTranslucentStatusBar(false)
-//                .withActionBarDrawerToggle(true)
-//                .withActionBarDrawerToggleAnimated(true)
-//                .addDrawerItems(App.item1, App.item2, App.item3, App.item4, App.item5, App.item6, App.item7)
-//                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-//                    @Override
-//                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-//                        Intent mIntent;
-//                        switch (position) {
-//                            case 0:
-//                                mIntent = new Intent(getApplicationContext(), MainActivity.class);
-//                                mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                                getApplicationContext().startActivity(mIntent);
-//                                break;
-//                            case 1:
-//                                mIntent = new Intent(getApplicationContext(), ScavengerHunt.class);
-//                                mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                                getApplicationContext().startActivity(mIntent);
-//                                break;
-//                            case 2:
-//                                mIntent = new Intent(getApplicationContext(), Schedule.class);
-//                                mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                                getApplicationContext().startActivity(mIntent);
-//                                break;
-//                            case 3:
-//                                mIntent = new Intent(getApplicationContext(), Map.class);
-//                                mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                                getApplicationContext().startActivity(mIntent);
-//                                break;
-//                            case 4:
-//                                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.alcheringa.in/registrations"));
-//                                startActivity(browserIntent);
-//                                break;
-//                            case 5:
-//                                mIntent = new Intent(getApplicationContext(), Sponsors.class);
-//                                mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                                getApplicationContext().startActivity(mIntent);
-//                                break;
-//                            case 6:
-//                                mIntent = new Intent(getApplicationContext(), ContactUs.class);
-//                                mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                                getApplicationContext().startActivity(mIntent);
-//                                break;
-//                        }
-//                        return true;
-//                    }
-//                })
-//                .build();
 
         Drawer drawer = new DrawerBuilder()
                 .withActivity(this)
@@ -126,52 +77,124 @@ public class ContactUs extends AppCompatActivity {
     }
 
     public void onClickCall1(View view) {
-        Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("tel:8399076595"));
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        startActivity(callIntent);
-
+    call1();
     }
+    public void call1()
+    {
+        try {
+            if (Build.VERSION.SDK_INT > 22) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
 
+                    ActivityCompat.requestPermissions(ContactUs.this, new String[]{Manifest.permission.CALL_PHONE}, 100);
+
+                    return;
+                }
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + "8399076595"));
+                startActivity(callIntent);
+
+            } else {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + "8399076595"));
+                startActivity(callIntent);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
     public void onClickCall2(View view) {
-        Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("tel:7638038129"));
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        startActivity(callIntent);
+
+        call2();
     }
+     public void call2()
+     {
+         try {
+             if (Build.VERSION.SDK_INT > 22) {
+                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                     // TODO: Consider calling
+
+                     ActivityCompat.requestPermissions(ContactUs.this, new String[]{Manifest.permission.CALL_PHONE}, 101);
+
+                     return;
+                 }
+
+                 Intent callIntent = new Intent(Intent.ACTION_CALL);
+                 callIntent.setData(Uri.parse("tel:" + "7638038129"));
+                 startActivity(callIntent);
+
+             } else {
+                 Intent callIntent = new Intent(Intent.ACTION_CALL);
+                 callIntent.setData(Uri.parse("tel:" + "7638038129"));
+                 startActivity(callIntent);
+             }
+         } catch (Exception ex) {
+             ex.printStackTrace();
+         }
+     }
+
 
     public void onClickCall3(View view) {
-        Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("tel:7896889515"));
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        startActivity(callIntent);
-
+        call3();
     }
+    public void call3()
+    {
+        try {
+            if (Build.VERSION.SDK_INT > 22) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+
+                    ActivityCompat.requestPermissions(ContactUs.this, new String[]{Manifest.permission.CALL_PHONE}, 102);
+
+                    return;
+                }
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + "7896889515"));
+                startActivity(callIntent);
+
+            } else {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + "7896889515"));
+                startActivity(callIntent);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults)
+    {
+
+
+        if(requestCode == 100)
+        {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                call1();
+            }
+        }
+
+
+        if(requestCode == 101)
+        {
+            if(grantResults[1] == PackageManager.PERMISSION_GRANTED)
+            {
+                call2();
+            }
+        }
+        if(requestCode == 102)
+        {
+            if(grantResults[2] == PackageManager.PERMISSION_GRANTED)
+            {
+                call3();
+            }
+        }
+    }
+
+
+
 }
